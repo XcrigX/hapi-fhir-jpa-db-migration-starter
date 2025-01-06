1668266bbaa8082a482d7d33045a6ba97f296406
craig mcclendon <cgithub@loudog.org>
2025-01-06 15:37:56 -0600

    added some springboot autoconfig exclusions

--- src/main/java/com/github/xcrigx/hapi/migratedb/config/SpringConfig.java
+++ src/main/java/com/github/xcrigx/hapi/migratedb/config/SpringConfig.java
@@ -1,11 +1,15 @@
 package com.github.xcrigx.hapi.migratedb.config;
 
 import com.github.xcrigx.hapi.migratedb.rest.PackageScanDummy;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @ComponentScan(basePackageClasses = { PackageScanDummy.class })
 @Configuration
+@SpringBootApplication(exclude = {ElasticsearchRestClientAutoConfiguration.class, ThymeleafAutoConfiguration.class})
 //@Import(WebMvcAutoConfiguration.class)
 //@EnableConfigurationProperties(ApplicationProperties.class)
 public class SpringConfig {
