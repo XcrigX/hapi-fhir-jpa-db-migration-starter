638069c30eea7ba9066d2e987ec78b07cecf9506
craig mcclendon <cgithub@loudog.org>
2025-01-07 17:06:29 -0600

    prevent quartz from starting

--- src/main/java/com/github/xcrigx/hapi/migratedb/config/SpringConfig.java
+++ src/main/java/com/github/xcrigx/hapi/migratedb/config/SpringConfig.java
@@ -3,13 +3,14 @@ package com.github.xcrigx.hapi.migratedb.config;
 import com.github.xcrigx.hapi.migratedb.rest.PackageScanDummy;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchRestClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @ComponentScan(basePackageClasses = { PackageScanDummy.class })
 @Configuration
-@SpringBootApplication(exclude = {ElasticsearchRestClientAutoConfiguration.class, ThymeleafAutoConfiguration.class})
+@SpringBootApplication(exclude = {ElasticsearchRestClientAutoConfiguration.class, ThymeleafAutoConfiguration.class,  QuartzAutoConfiguration.class })
 //@Import(WebMvcAutoConfiguration.class)
 //@EnableConfigurationProperties(ApplicationProperties.class)
 public class SpringConfig {
